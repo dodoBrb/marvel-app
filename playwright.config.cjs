6c363dad90840dce6682e2c21680a7a44af4a299
Dorine Barbey <dorinebarbey33@gmail.com>
2024-11-27 18:11:51 +0100

    Enable local dev server in Playwright configuration

--- playwright.config.cjs
+++ playwright.config.cjs
@@ -71,10 +71,10 @@ module.exports = defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run dev',
-  //   url: 'http://localhost:5173',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
+  },
 });
 
